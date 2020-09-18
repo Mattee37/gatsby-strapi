fd4f48bff5494e7f9dc21982c92b31c7f1d65d08
Mattee37 <bluestacksmm.137@gmail.com>
2020-09-18 14:48:14 -0300

    Third commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Test con Strapi`,
+    description: `Una simple PWA hecha con Gastby y Stapi!`,
+    author: `Mattee37`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,29 +16,23 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Test con Strapi`,
+        short_name: `Test con Strapi`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: `#ffffff`,
+        theme_color: `#000000`,
+        display: `standalone`,
+        icon: `src/images/gatsby-icon.png`,
       },
     },
     {
       resolve: "gatsby-plugin-apollo",
       options: {
-        // uri: "https://bienesraices-137.herokuapp.com/graphql",
         uri: "https://bienesraices-137.herokuapp.com/graphql",
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
